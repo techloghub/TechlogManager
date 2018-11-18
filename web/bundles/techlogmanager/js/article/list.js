3d0efa4e50ce6da8080f38d38067a5c2795c1e68
刘泽宇 <liuzeyu@dibgroup.cn>
2018-11-18 10:35:05 +0800

    抽象化 js

--- web/bundles/techlogmanager/js/article/list.js
+++ web/bundles/techlogmanager/js/article/list.js
@@ -10,21 +10,6 @@ $(document).ready(function() {
             $('#start_' + key).datetimepicker(datetimeConfig);
             $('#end_' + key).datetimepicker(datetimeConfig);
         }
-    } else {
-        $('#start_insert_time').datetimepicker(datetimeConfig);
-        $('#end_insert_time').datetimepicker(datetimeConfig);
-        $('#start_update_time').datetimepicker(datetimeConfig);
-        $('#end_update_time').datetimepicker(datetimeConfig);
-        $('#start_finish_time').datetimepicker(datetimeConfig);
-        $('#end_finish_time').datetimepicker(datetimeConfig);
-        $('#start_date').datetimepicker(datetimeConfig);
-        $('#end_date').datetimepicker(datetimeConfig);
-        $('#start_inserttime').datetimepicker(datetimeConfig);
-        $('#end_inserttime').datetimepicker(datetimeConfig);
-        $('#start_updatetime').datetimepicker(datetimeConfig);
-        $('#end_updatetime').datetimepicker(datetimeConfig);
-        $('#start_date').datetimepicker(datetimeConfig);
-        $('#end_date').datetimepicker(datetimeConfig);
     }
     hideProgress();
 
