dd39805ce37eec0fa53e7995f82e7f32f055cb75
liuzeyu <liuzeyu@dibgroup.cn>
2019-07-22 23:00:49 +0800

    更改默认排序

--- src/Manager/TechlogBundle/Repository/CalendarAlertRepository.php
+++ src/Manager/TechlogBundle/Repository/CalendarAlertRepository.php
@@ -34,7 +34,7 @@ class CalendarAlertRepository extends EntityRepository
 				$where_sql .= ' desc';
 			$where_sql .= ', id desc';
 		} else {
-			$where_sql .= ' order by id desc';
+			$where_sql .= ' order by next_time desc';
 		}
 
         $total_sql .= $where_sql;
