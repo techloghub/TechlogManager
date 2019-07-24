7d61de8e497d3abb2b6b8c62235db202b25291ad
liuzeyu <liuzeyu@dibgroup.cn>
2019-07-24 13:54:44 +0800

    新增日历提醒状态更新接口

--- src/Manager/ApiBundle/Controller/CalendarController.php
+++ src/Manager/ApiBundle/Controller/CalendarController.php
@@ -3,7 +3,12 @@
 
 namespace Manager\ApiBundle\Controller;
 
+use Component\Library\LunarHelper;
 use Exception;
+use Manager\TechlogBundle\Entity\CalendarAlert;
+use Manager\TechlogBundle\ManagerTechlogBundle;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -11,7 +16,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 /**
  * @Route("/calendar")
  */
-class CalendarController
+class CalendarController extends Controller
 {
     /**
      * @Route("/setalerttime");
@@ -24,6 +29,18 @@ class CalendarController
         \date_default_timezone_set('PRC');
 
         $id = $request->get('id');
-        return null;
+        $date = date('Y-m-d H:i:s');
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $entity = $em->getRepository('ManagerTechlogBundle:CalendarAlert')->findOneById($id);
+        if (empty($entity)) {
+            return new JsonResponse(array('code' => 1, 'msg' => 'id is wrong'));
+        }
+
+        $entity->setAltertTime($date);
+        $entity->setNextTime(LunarHelper::getNextAlert($entity));
+        $em->persist($entity);
+        $em->flush();
+        return  new JsonResponse(array('code' => 1, 'msg' => '更新成功'));
     }
 }
